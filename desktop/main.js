dc9ed291374b667a9a2d3210b57b6e10ea6cdcb7
Alexis Azanza <alzayon@gmail.com>
2018-09-13 00:24:01 +0800

    Initial commit
    Electron shell and Ionic blank already working

---
+++ desktop/main.js
@@ -0,0 +1,47 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win 
+
+//Called when Electron has finished initialization and ready to create browser windows
+app.on('ready', createWindow)
+
+function createWindow() {
+
+    win = new BrowserWindow({
+        width: 1200, 
+        height: 800,
+        title: 'Bookmarker'
+    })
+
+    //Open the dev tools
+    win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win = null
+    })
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '../www/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+}
+
+app.on('window-all-closed', () => {
+    //For mac os, it is common for applications to have their menu active even
+    //though all windows for them are now closed
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    //Only on Mac, when dock icon is clicked and no window for the app is already open
+    if (win == null) {
+        createWindow()
+    }
+})
+
